e1161d654373886fcafd860f811101d203c852bf
AkhilAajneeti <akhilaajneeti@gmail.com>
2025-12-13 15:24:01 +0530

    add next page content so make changes in out team api

--- src/api/teamApi.js
+++ src/api/teamApi.js
@@ -1,6 +1,18 @@
 import api from "./axiosConfig";
 
 export const getTeamMembers = async () => {
-  const response = await api.get("/team-members/");
-  return response.data.results || [];
+  // const response = await api.get("/team-members/");
+  // return response.data.results || [];
+  let allResults = [];
+  let nextUrl = "/team-members/";
+
+  while (nextUrl) {
+    const response = await api.get(nextUrl);
+    allResults = [...allResults, ...response.data.results];
+    nextUrl = response.data.next
+      ? response.data.next.replace("https://jenikaventures.com/api", "")
+      : null;
+  }
+
+  return allResults;
 };
