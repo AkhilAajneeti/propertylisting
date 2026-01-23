51cb5d45e406278ba371f5d295f4103cb350245e
AkhilAajneeti <akhilaajneeti@gmail.com>
2026-01-23 17:24:47 +0530

    update backend api

--- src/components/chatbot/SparkleIcon.jsx
+++ src/components/chatbot/SparkleIcon.jsx
@@ -1,5 +1,5 @@
 const SparkleIcon = ({ size = 28 }) => (
-<img src="public/sparkle.png" alt=""width={size} height={size} />
+<img src="sparkle.png" alt=""width={size} height={size} />
 );
 
 export default SparkleIcon;
